e4ab7371f4a4477ebef8684815e6797dae02463f
FarronF <farron.flaherty@hmcts.net>
2021-12-21 16:59:42 +0000

    Adding ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,6 +18,7 @@ import { ShoppingReducer } from './store/reducers/shopping.reducer';
     StoreModule.forRoot({
       shopping: ShoppingReducer
     }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
