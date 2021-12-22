6e7a06cd9f85d4de9216264f51d60ee6e5c0b597
FarronF <farron.flaherty@hmcts.net>
2021-12-22 11:45:45 +0000

    Adding code for shopping service calls

--- src/app/shopping.service.ts
+++ src/app/shopping.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { delay } from 'rxjs';
+import { ShoppingItem } from './store/models/shopping-item.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
   private SHOPPING_URL = 'http://localhost:3000/shopping'
+  private DELAY_TIME = 500;
 
   constructor(private http: HttpClient) { }
 
   getShoppingItems() {
-
+    return this.http.get(this.SHOPPING_URL).pipe(delay(this.DELAY_TIME));
   }
 
-  addShoppingItem() {
-
+  addShoppingItem(shoppingItem: ShoppingItem) {
+    return this.http.post(this.SHOPPING_URL, shoppingItem).pipe(delay(this.DELAY_TIME));
   }
 
-  deleteShoppingItem() {
-    
+  deleteShoppingItem(id: string) {
+    return this.http.delete(`this.SHOPPING_URL/${id}`).pipe(delay(this.DELAY_TIME));
   }
 }
